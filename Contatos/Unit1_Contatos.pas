f65d9c09c8c2681da8d63167c7e01ab503fa3cc6
farleybsd <farley.t.i@hotmail.com>
2020-08-12 19:51:22 -0300

    Projeto Contatos e Criacao da tabela

---
+++ Contatos/Unit1_Contatos.pas
@@ -0,0 +1,24 @@
+unit Unit1_Contatos;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFORM_CONTATOS = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FORM_CONTATOS: TFORM_CONTATOS;
+
+implementation
+
+{$R *.dfm}
+
+end.
